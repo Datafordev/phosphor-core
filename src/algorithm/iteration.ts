049e719355786f7461e123c4686b9a9aa6fe9ef9
S. Chris Colbert <sccolbert@gmail.com>
2016-03-01 16:33:17 -0500

    add forEach iteration function

---
+++ src/algorithm/iteration.ts
@@ -0,0 +1,35 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2016, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+import {
+  IInputRange
+} from '../range/types';
+
+
+/**
+ * Invoke a function for each value in a range.
+ *
+ * @param range - The input range of values to iterate.
+ *
+ * @param fn - The callback function to invoke for each value.
+ *
+ * #### Notes
+ * This function greedily iterates the range and invokes the callback
+ * once for each value. Iteration cannot be terminated early.
+ *
+ * The callback is provided the value and the zero-based index of the
+ * current iteration.
+ *
+ * The callback should not modify the length of the range.
+ */
+export
+function forEach<T>(range: IInputRange<T>, fn: (value: T, index: number) => void): void {
+  for (let i = 0; !range.isEmpty(); ++i) {
+    fn(range.front(), i);
+    range.popFront();
+  }
+}
