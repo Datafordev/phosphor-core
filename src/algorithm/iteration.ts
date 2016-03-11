b4cbc713010a2a551e8c8e4f0c8db19df6758ef9
S. Chris Colbert <sccolbert@gmail.com>
2016-03-10 19:23:51 -0600

    add 'some' and 'every' iterators

--- src/algorithm/iteration.ts
+++ src/algorithm/iteration.ts
@@ -23,12 +23,60 @@ import {
  *
  * The callback is provided the value and the zero-based index of the
  * current iteration.
- *
- * The callback should not modify the length of the range.
  */
 export
 function each<T>(range: IInputRange<T>, fn: (value: T, index: number) => void): void {
   for (let i = 0; !range.isEmpty(); ++i) {
     fn(range.popFront(), i);
   }
+}
+
+
+/**
+ * Test whether all values in a range satisfy a predicate.
+ *
+ * @param range - The input range of values to iterate.
+ *
+ * @param fn - The predicate function to invoke for each value.
+ *
+ * @returns `true` if all values pass, `false` otherwise.
+ *
+ * #### Notes
+ * This function greedily iterates the range and invokes the predicate
+ * once for each value. Iteration terminates on the first non-match.
+ *
+ * The predicate is provided the value and the zero-based index of the
+ * current iteration.
+ */
+export
+function every<T>(range: IInputRange<T>, fn: (value: T, index: number) => boolean): boolean {
+  for (let i = 0; !range.isEmpty(); ++i) {
+    if (!fn(range.popFront(), i)) return false;
+  }
+  return true;
+}
+
+
+/**
+ * Test whether any value in a range satisfies a predicate.
+ *
+ * @param range - The input range of values to iterate.
+ *
+ * @param fn - The predicate function to invoke for each value.
+ *
+ * @returns `true` if any value passes, `false` otherwise.
+ *
+ * #### Notes
+ * This function greedily iterates the range and invokes the predicate
+ * once for each value. Iteration terminates on the first match.
+ *
+ * The predicate is provided the value and the zero-based index of the
+ * current iteration.
+ */
+export
+function some<T>(range: IInputRange<T>, fn: (value: T, index: number) => boolean): boolean {
+  for (let i = 0; !range.isEmpty(); ++i) {
+    if (fn(range.popFront(), i)) return true;
+  }
+  return false;
 }
