47bcd08f43b174a45367a250dbe434e59baec9b8
S. Chris Colbert <sccolbert@gmail.com>
2016-03-18 10:13:17 -0500

    move IIterator interface to separate file

--- src/algorithm/iteration.ts
+++ src/algorithm/iteration.ts
@@ -5,48 +5,9 @@
 |
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
-
-
-/**
- * An object which traverses a collection of values.
- */
-export
-interface IIterator<T> {
-  /**
-   * Create an independent clone of the current iterator.
-   *
-   * @returns A new independent clone of the current iterator.
-   *
-   * #### Notes
-   * The cloned iterator can be consumed independently of the current
-   * iterator. In essence, it is a copy of the iterator value stream
-   * which starts at the current iterator location. This can be useful
-   * for iterator lookahead and stream duplication.
-   *
-   * Most iterators can trivially support cloning. Those which cannot
-   * (a single pass iterator) should throw an exception and document
-   * that they cannot be cloned.
-   */
-  clone(): IIterator<T>;
-
-  /**
-   * Get the next value in the collection.
-   *
-   * @returns The next value in the collection, or `undefined` if the
-   *   iterator is exhausted.
-   *
-   * #### Notes
-   * The `undefined` value is used to signal the end of iteration and
-   * should therefore not be used as a value in a collection.
-   *
-   * The use of the `undefined` sentinel is an explicit design choice
-   * which favors performance over purity. The ES6 method of returning
-   * a `{ value, done }` pair is undesirable as it requires an object
-   * allocation on each iteration, and an `isDone()` method increases
-   * implementation and runtime complexity.
-   */
-  next(): T;
-}
+import {
+  IIterator
+} from './types';
 
 
 /**
--- src/patterns/disposable.ts
+++ src/patterns/disposable.ts
@@ -6,8 +6,12 @@
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
 import {
-  IIterator, each
+  each
 } from '../algorithm/iteration';
+
+import {
+  IIterator
+} from '../algorithm/types';
 
 import {
   assert
