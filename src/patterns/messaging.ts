b647b11f51be9980e07aaed17dc8deba7028daea
S. Chris Colbert <sccolbert@gmail.com>
2016-03-24 17:14:06 -0500

    add conflatable message convenience class

--- src/patterns/messaging.ts
+++ src/patterns/messaging.ts
@@ -112,6 +112,45 @@ class Message {
 
 
 /**
+ * A convenience message class which automatically conflates.
+ *
+ * #### Notes
+ * Message conflation is an advanced topic. Most user code will not
+ * make use of this class.
+ *
+ * This message class is useful for creating message instances which
+ * should be conflated, but which have no state other than `type`.
+ *
+ * If conflation of stateful messages is required, a custom `Message`
+ * subclass should be created.
+ */
+export
+class ConflatableMessage {
+  /**
+   * Test whether the message is conflatable.
+   *
+   * #### Notes
+   * This property is always `true`.
+   *
+   * This is a read-only property.
+   */
+  get isConflatable(): boolean {
+    return true;
+  }
+
+  /**
+   * Conflate this message with another message of the same `type`.
+   *
+   * #### Notes
+   * This method always returns `true`.
+   */
+  conflate(other: ConflatableMessage): boolean {
+    return true;
+  }
+}
+
+
+/**
  * An object which handles messages.
  *
  * #### Notes
