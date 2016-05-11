d771111c247cc13dbeff14984f5c8dc3d009ebe5
S. Chris Colbert <sccolbert@gmail.com>
2016-05-11 11:07:52 -0500

    add string search algorithm

--- src/algorithm/searching.ts
+++ src/algorithm/searching.ts
@@ -405,3 +405,98 @@ function upperBound<T, U>(object: SequenceOrArrayLike<T>, value: U, fn: (element
   }
   return begin;
 }
+
+
+/**
+ * A namespace which holds string searching functionality.
+ */
+export
+namespace StringSearch {
+  /**
+   * The result of a sum-of-squares string search.
+   */
+  export
+  interface ISumOfSquaresResult {
+    /**
+     * A score which indicates the strength of the match.
+     *
+     * A lower score is better. Zero is the best possible score.
+     */
+    score: number;
+
+    /**
+     * The indices of the matched characters in the source text.
+     *
+     * The indices will appear in increasing order.
+     */
+    indices: number[];
+  }
+
+  /**
+   * Compute the sum-of-squares match for the given search text.
+   *
+   * @param sourceText - The text which should be searched.
+   *
+   * @param queryText - The query text to locate in the source text.
+   *
+   * @returns The match result object, or `null` if there is no match.
+   *
+   * #### Complexity
+   * Linear on `sourceText`.
+   *
+   * #### Notes
+   * This scoring algorithm uses a sum-of-squares approach to determine
+   * the score. In order for there to be a match, all of the characters
+   * in `queryText` **must** appear in `sourceText` in order. The index
+   * of each matching character is squared and added to the score. This
+   * means that early and consecutive character matches are preferred.
+   *
+   * The character match is performed with strict equality. It is case
+   * sensitive and does not ignore whitespace. If those behaviors are
+   * required, the text should be transformed before scoring.
+   */
+  export
+  function sumOfSquares(sourceText: string, queryText: string): ISumOfSquaresResult {
+    let score = 0;
+    let indices = new Array<number>(queryText.length);
+    for (let i = 0, j = 0, n = queryText.length; i < n; ++i, ++j) {
+      j = sourceText.indexOf(queryText[i], j);
+      if (j === -1) {
+        return null;
+      }
+      indices[i] = j;
+      score += j * j;
+    }
+    return { score, indices };
+  }
+
+  /**
+   * Highlight the matched characters of a source string.
+   *
+   * @param source - The text which should be highlighted.
+   *
+   * @param indices - The indices of the matched characters. They must
+   *   appear in increasing order and must be in bounds of the source.
+   *
+   * @returns A string with interpolated `<mark>` tags.
+   */
+  export
+  function highlight(sourceText: string, indices: number[]): string {
+    let k = 0;
+    let last = 0;
+    let result = '';
+    let n = indices.length;
+    while (k < n) {
+      let i = indices[k];
+      let j = indices[k];
+      while (++k < n && indices[k] === j + 1) {
+        j++;
+      }
+      let head = sourceText.slice(last, i);
+      let chunk = sourceText.slice(i, j + 1);
+      result += `${head}<mark>${chunk}</mark>`;
+      last = j + 1;
+    }
+    return result + sourceText.slice(last);
+  }
+}
